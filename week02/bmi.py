2e5c6ff9dcb62dafb14cdd99984d3755e3f1b957
Ruth McQuillan <77881700+G00398347@users.noreply.github.com>
2021-02-07 20:19:33 +0000

    Update bmi.py

--- week02/bmi.py
+++ week02/bmi.py
@@ -3,7 +3,7 @@
 
 strweight = input ('Enter your weight in kilograms: ')           # this line asks for the persons weight in kgs
 strheight = input ('Enter your height in centimetres: ')         # ditto for height in cms
-heightinmeters= float(strheight)/100                             # converts input string height to float and calculates height in metres
+heightinmetres= float(strheight)/100                             # converts input string height to float and calculates height in metres
 weight = float(strweight)                                        # converts input string weight to float
-bmi = round( weight/(heightinmeters ** 2 ),2)                    # performs bmi calculation and rounds to 2 decimal places
+bmi = round( weight/(heightinmetres ** 2 ),2)                    # performs bmi calculation and rounds to 2 decimal places
 print ('Your BMI is: {}. ' .format (bmi) )                       # outputs calculation to screen
