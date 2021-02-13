114e850756292b6555b23fc7d69e99779879481d
Ruth McQuillan <ruthpmcquillan@gmail.com>
2021-02-13 17:41:16 +0000

    week02

---
+++ week02/lab2.3.1testTypes.py
@@ -0,0 +1,16 @@
+#This programme uses type function  in python to check variable types
+#Author : Ruth McQuillan
+
+#varialbes are assigned names
+i = 3
+fl = 3.5
+isa = True
+memo = "how now Brown Cow"
+lots = []
+
+#command to output the variable name, type and value
+print ("variable {} is of type:{} and value:{}" .format ("i", type(i), i))
+print ("variable {} is of type:{} and value:{}" .format ("fl", type(fl), fl))
+print ("variable {} is of type:{} and value:{}" .format ("isa", type(isa), isa))
+print ("variable {} is of type {} and value:{}" .format ("memo", type(memo),memo))
+print ("variable {} is of type {} and value:{}" .format ("lots", type(lots),lots))
