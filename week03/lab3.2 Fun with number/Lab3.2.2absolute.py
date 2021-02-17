950067095ac585e3751147c8a3c194618e5be4ec
Ruth McQuillan <ruthpmcquillan@gmail.com>
2021-02-17 21:43:46 +0000

    week02

--- week03/lab3.2 Fun with number/Lab3.2.2absolute.py
+++ week03/lab3.2 Fun with number/Lab3.2.2absolute.py
@@ -1,6 +1,8 @@
 #this programme displays the absolute value of the number input by the user
 #the given answer has a decimal point
 #therfore I use float as the input data type
+#Author: Ruth McQuillan
+
 number = float (input ("Enter a number:"))
 absoluteValue = abs(number)
 print ('The absolute value of {} is {}'.format (number, absoluteValue))
