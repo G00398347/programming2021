fe34b249c7ba02439987d4486589080913f1b8ce
Ruth McQuillan <ruthpmcquillan@gmail.com>
2021-02-21 14:50:46 +0000

    week03

--- week03/lab3.2 Fun with number/Lab3.2.1round.py
+++ week03/lab3.2 Fun with number/Lab3.2.1round.py
@@ -1,9 +1,14 @@
-#this programme rounds a number
+#This programme rounds a number
 #Author: Ruth McQuillan
 
-numberToRound = float (input ("Enter a float number:"))              # This asks for a number with a decimal point
-roundedNumber = round(numberToRound)                                 # This uses the round built in function to round the float number entered at input
+numberToRound = float (input ("Enter a float number:"))               #this asks for a number with a decimal point
+roundedNumber = round(numberToRound)                                  #this uses the round built in function to round the float number entered at input
 
 
-#The soloution rounds to the nearest EVEN number- Be careful of round
+#I have found inconsitency in whether a float gets rounded up or down
+#it does not seem to round to the nearest even number as stated in the lab
+#for example 7.1 rounded to 7 while 7.5 rounded to 8 when I ran this
+#it appears to be more to do with wheter you are above at or below .5
+#be careful of round if accuracy is NB.
+
 print ( '{} rounded is {}'.format (numberToRound,roundedNumber))     
