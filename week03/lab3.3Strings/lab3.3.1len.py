e93142432b92ea50df1b1503c3a3c52c3e0415d1
Ruth McQuillan <ruthpmcquillan@gmail.com>
2021-02-21 17:12:39 +0000

    week03

---
+++ week03/lab3.3Strings/lab3.3.1len.py
@@ -0,0 +1,10 @@
+#This programme returns the lenght of the input string
+#Author: Ruth McQuillan
+
+str1 = input ("Please enter a string: ")                             #required input in string format
+lenStr1 = len (str1)                                                 #counts the number of characters in the input string
+print ("The length of {} is {} characters " .format (str1, lenStr1)) #outputs the count of characters in the requested sentance
+                                                                     #assuming that the answer code given ommited the word 'characters' 
+                                                                     #in the print command inadvertently
+
+                                                                     
---
+++ week03/lab3.3Strings/lab3.3.2tab.py
@@ -0,0 +1,5 @@
+#This programme is creating and outputting a string
+#Author: Ruth McQuillan
+
+message = 'John said\t"Hi"\nI said\t\t"bye"'  #requried:  string including tabs, and new line
+print (message)                               #output command to print message
